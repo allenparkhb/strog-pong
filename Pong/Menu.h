ca8d8914bbff61641544febdb37fe10d26c76415
StrogAnon <StrogAnon@e8402ffd-162d-859a-42b0-96b8e9fa5dff>
2011-01-24 19:13:03 +0000

    Fixed the menu system. Problem was that I needed to have prototypes in Renderer and Menu for Menu and Button respectively. Also made the Button array in Menu dynamic. Reversed the images because I made them wrong originally. Renderer can now handle rendering the different states of the menu, as well as the game.

--- Pong/Menu.h
+++ Pong/Menu.h
@@ -2,17 +2,22 @@
 
 #include "Button.h"
 
+class Button;
+
 class Menu
 {
 public:
 	TexturePack m_mainBackground;
 	TexturePack m_CreditsBackground;
 	bool changedButtonTexture;		// used to determine if a button's texture has already been changed
+	~Menu();
 
-	Button m_Buttons[4];
-
-	bool isOnMain;					// flags if the menu is displaying the main menu or the credits
+	Button* m_Buttons;
+	eMenuStates eMState;		// flags if the menu is displaying the main menu or the credits
+	eButton m_result;				// result of what button (if any) was pressed in the main menu
 	int numButtons;
 	void Init(ScreenDim dims);
 	eButton Menu::Update(bool mousePressed, int mousePosX, int mousePosY);
+
+
 };
